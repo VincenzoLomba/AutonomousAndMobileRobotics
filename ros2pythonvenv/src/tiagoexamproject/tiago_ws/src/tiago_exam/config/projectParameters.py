d4cd249160c51ea5ce6c7d3c0fa67c68c3e9af75
Vincenzo Lombardi <vincenzolombardi99@gmail.com>
2026-04-13 16:50:23 +0200

    Generated a new custom ament_python package to be used for all the project tasks and named tiago_exam_tasks
    Also moved within this package the already defined launcher for the Task1 and the related parameters file

--- ros2pythonvenv/src/tiagoexamproject/tiago_ws/src/tiago_exam/config/projectParameters.py
+++
@@ -1,4 +0,0 @@
-
-# This file contains all the parameters that are gonna be used in the project
-
-worldName = 'group7' # the name of the Gazebo world to be used the world file, which is "group7.world")
