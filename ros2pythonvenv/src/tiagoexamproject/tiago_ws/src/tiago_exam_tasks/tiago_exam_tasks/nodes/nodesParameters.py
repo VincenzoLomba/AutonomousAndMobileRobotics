24c92e5466aa529cb7044c3d0f67706f298baccf
Vincenzo Lombardi <vincenzolombardi99@gmail.com>
2026-04-14 16:33:45 +0200

    Modified home pose for Tiago

--- ros2pythonvenv/src/tiagoexamproject/tiago_ws/src/tiago_exam_tasks/tiago_exam_tasks/nodes/nodesParameters.py
+++ ros2pythonvenv/src/tiagoexamproject/tiago_ws/src/tiago_exam_tasks/tiago_exam_tasks/nodes/nodesParameters.py
@@ -4,6 +4,7 @@
 
 tiagoArmActionName = "tiago_arm_action"
 
+"""
 HOME_JOINT_POSITIONS = [
     0.096,
     2.460914,
@@ -13,4 +14,15 @@ HOME_JOINT_POSITIONS = [
     -0.942478,
     0.069813,
     -1.047198,
+]
+"""
+HOME_JOINT_POSITIONS = [
+    0.0,       # torso_lift_joint   0.0m
+    2.478368,  # arm_1_joint        142°
+    -1.343904, # arm_2_joint        -77°
+    0.506145,  # arm_3_joint        29°
+    1.972222,  # arm_4_joint        113°
+    -1.745329, # arm_5_joint        -100°
+    1.396263,  # arm_6_joint        80°
+    0.226893,  # arm_7_joint        13°
 ]
